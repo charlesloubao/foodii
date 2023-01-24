d88608bc354c99a7ac96f24e52838fb039fa9224
Charles Eugene Loubao <charlesloubao95@gmail.com>
2023-01-24 07:07:19 -0500

    Added automatic order fulfilment

---
+++ apps/restaurateur/README.md
@@ -0,0 +1,9 @@
+# Restaurateur
+
+![Restaurateur](https://images.unsplash.com/photo-1512485800893-b08ec1ea59b1?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80)
+Photo by [Petr Sevcovic on Unsplash](https://unsplash.com/photos/e5Q5vWO55uU)
+
+Hi!
+I am Restaurateur 🤖 my only job is to simulate order
+fulfillment when you place an order on [Foodii](https://foodii.vercel.app)
+using [Supabase Realtime API](https://supabase.com/docs/guides/realtime)
---
+++ apps/restaurateur/src/index.ts
@@ -0,0 +1,43 @@
+import dotenv from 'dotenv'
+import {SupabaseClient} from "@supabase/supabase-js";
+
+dotenv.config()
+
+const supabaseClient = new SupabaseClient(process.env.SUPABASE_URL!, process.env.SUPABASE_SERVER_ROLE_KEY!)
+
+
+function processOrder(order: any) {
+    console.log("New order", order.id)
+    setTimeout(() => {
+        return supabaseClient.from("orders")
+            .update({status: "out_for_delivery"})
+            .eq("id", order.id)
+            .then(({error}) => {
+                if(error) throw error
+            })
+    }, 2000)
+
+    setTimeout(() => {
+        return supabaseClient.from("orders")
+            .update({status: "delivered"})
+            .eq("id", order.id)
+            .then(({error}) => {
+                if(error) throw error
+            })
+    }, 4000)
+}
+
+supabaseClient.channel(`*`)
+    .on("postgres_changes",
+        {
+            event: "INSERT",
+            schema: "public",
+            table: "orders",
+        },
+        (event) => {
+            const order = event.new
+            processOrder(order)
+        }).subscribe((status, err) => {
+    if (err) throw err
+    console.log({status})
+})
